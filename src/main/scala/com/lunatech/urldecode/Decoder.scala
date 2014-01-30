0bd61e12d4a16f52983cd1da356b281d7ca2c32b
Vincent Grente <vgrente@gmail.com>
2014-01-30 14:18:29 +0100

    changed basd on comments made by bart

--- src/main/scala/com/lunatech/urldecode/Decoder.scala
+++ src/main/scala/com/lunatech/urldecode/Decoder.scala
@@ -1,23 +1,65 @@
 package com.lunatech.urldecode
 
 import java.nio.charset.Charset
+import java.net.URISyntaxException
 
 object Decoder {
 
-  val pattern = """(%[0-9a-fA-F]{2})+""".r
   val charset = Charset.forName("UTF-8")
 
-  def decode(str: String): String = {
-    pattern.replaceAllIn(str, x => Decoder.decodeBytes(x.toString.split("%").tail.toList))
-  }
+  def decode2(str: String): String = {
+    val encoded = str.getBytes(charset)
+    val decoded = new Array[Byte](encoded.length)
+    var i = 0
+    var j: Int = 0
+    for (x <- 0 until encoded.length) {
+       if (x == i) {
+        if (encoded(i) == '%') {
+          if (i + 2 >= encoded.length)
+            throw new URISyntaxException(str, "Invalid URL-encoded string at char " + i);
+          else {
+            i = i + 1
+            val first = encoded(i)
+            i = i + 1
+            val second = encoded(i)
+            i = i + 1
+            val value = fromHexToInt(first) * 16 + fromHexToInt(second)
+            decoded(j) = value.toByte
+          }
 
-  def fromHexToInt(hex: String): Int = {
-    Integer.parseInt(hex, 16)
-  }
+        } else {
+          decoded(j) = encoded(i)
+          i =i +1
+        }
+        j = j + 1
+      }
+    }
+    new String(decoded, 0, j, charset)
+  }                                             
 
-  def decodeBytes(hexa: List[String]): String = {
-    val bytes = hexa.map(fromHexToInt(_)).map(_.toByte)
-    new String(bytes.toArray, 0, bytes.length, charset)
+  def fromHexToInt(b: Byte): Byte = b match {
+    case '0' => 0
+    case '1' => 1
+    case '2' => 2
+    case '3' => 3
+    case '4' => 4
+    case '5' => 5
+    case '6' => 6
+    case '7' => 7
+    case '8' => 8
+    case '9' => 9
+    case 'a' => 10
+    case 'A' => 10
+    case 'b' => 11
+    case 'B' => 11
+    case 'c' => 12
+    case 'C' => 12
+    case 'd' => 13
+    case 'D' => 13
+    case 'e' => 14
+    case 'E' => 14
+    case 'f' => 15
+    case 'F' => 15
+    case _ => throw new URISyntaxException(String.valueOf(b), "Invalid URL-encoded string");
   }
-
 }
--- src/test/scala/decode/DecodeTestSuite.scala
+++ src/test/scala/decode/DecodeTestSuite.scala
@@ -3,36 +3,44 @@ package decode
 import org.specs2.mutable._
 import com.lunatech.urldecode.Decoder._
 
+import java.net.URISyntaxException
+
 class DecodeTestSuite extends Specification {
 
   "The decoder " should {
     " let the normal strings unchanged" in {
-      decode("Just a normal string") mustEqual "Just a normal string"
+      decode2("Just a normal string") mustEqual "Just a normal string"
     }
   }
 
   "The decoder " should {
     " decode the path of an url and replace %-hexadecimal encoded characters " in {
-      decode("/download/images%E2%82%AC/fromage+pat%C3%A9.png") mustEqual "/download/images€/fromage+paté.png"
+      decode2("/download/images%E2%82%AC/fromage+pat%C3%A9.png") mustEqual "/download/images€/fromage+paté.png"
     }
   }
 
   "The decoder " should {
-    " let incorrect %-hexadecimal encoded characters unchanged" in {
-      decode("/download/images/fromage+pat%HQ.png") mustEqual "/download/images/fromage+pat%HQ.png"
+    " should throw an exception for incorrect %-hexadecimal " in {
+      decode2("/download/images/fromage+pat%HQ.png") must throwA[URISyntaxException]
     }
   }
 
   "The decoder " should {
-    " replace '%C3%A9' %-hexadecimal by é " in {
-      decodeBytes(List("C3", "A9")) mustEqual "é"
+    " replace '%25' %-hexadecimal by % " in {
+      decode2("%25") mustEqual "%"
     }
   }
-
+  
+  
   "The decoder " should {
-    " replace '%E2%82%AC' %-hexadecimal by é " in {
-      decodeBytes(List("E2", "82", "AC")) mustEqual "€"
+    " should throw an exception for %QQ " in {
+      decode2("%QQ") must throwA[URISyntaxException]
     }
   }
-
+  
+  "The decoder " should {
+    " should throw an exception for % " in {
+      decode2("%") must throwA[URISyntaxException]
+    }
+  }
 }
